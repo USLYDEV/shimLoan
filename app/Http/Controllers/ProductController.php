e2279ffbf682f9691aa4dc1be8684f164d264429
sylvesteraladejana@gmail.com <sylvesteraladejana@gmail.com>
2023-10-13 01:55:42 +0100

    i connected the create new product to submit data to database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -15,10 +16,23 @@ class ProductController extends Controller
     }
 
     public function store (Request $request){
-        dd($request);
+        // This dump data in frontend
+        // dd($request);
+    
+        // validate data before submission
+        $data = $request->validate([
+            'name' => 'required',
+            'quantity' => 'required | numeric',
+            'price' => 'required | decimal:2',
+            'description' => 'nullable',
+
+        ]);
+            //This save the create new product submission into database
+            $newProduct = Product::create($data);
+
+            //This auto return the user to homepage on submission
+            return redirect(route('products.index'));
         }
-
-
 }
 
 
